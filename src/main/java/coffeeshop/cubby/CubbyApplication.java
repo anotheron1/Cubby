171fd78f8276fa28f907cf511c66637ad4551e9f
anotheron1 <anotheron1@users.noreply.github.com>
2020-09-03 01:03:28 +0300

    init spring app

---
+++ src/main/java/coffeeshop/cubby/CubbyApplication.java
@@ -0,0 +1,13 @@
+package coffeeshop.cubby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coffeeshop/cubby/CubbyApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeeshop.cubby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
